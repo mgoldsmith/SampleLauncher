dc098976d827a22e139433309e53bbba9039642d
oMittens <matthewjg@mac.com>
2025-12-17 17:33:41 -0800

    Basic audio engine instantiation in app delegate

--- SampleLauncher/AppDelegate.m
+++ SampleLauncher/AppDelegate.m
@@ -5,22 +5,44 @@
 //  Created by Matthew Goldsmith on 12/16/25.
 //
 
+#import <AVFoundation/AVFoundation.h>
 #import "AppDelegate.h"
+#import "SampleBank.h"
+#import "SampleSlot.h"
 
 @interface AppDelegate ()
 
+@property (nonatomic, strong) AVAudioEngine *audioEngine;
+@property (nonatomic, strong) SampleBank *sampleBank;
 
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    // Initialize audio engine and its main mixer node
+    self.audioEngine = [[AVAudioEngine alloc] init];
+    [self.audioEngine mainMixerNode];
+
+    // Initialize sample bank with 16 slots
+    self.sampleBank = [[SampleBank alloc] init];
+
+     // Attach all sample slots to the audio engine
+    [self.sampleBank attachToAudioEngine:self.audioEngine];
+
+    // Start the audio engine
+    NSError *error = nil;
+    if (![self.audioEngine startAndReturnError:&error]) {
+        NSLog(@"Failed to start audio engine: %@", error);
+    }
+
+//    [[self.sampleBank slotAtIndex:1] play];
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    // Stop the audio engine
+    [self.audioEngine stop];
 }
 
 
