e45833dce35327ae03f5517d832cafcbcfce23f6
oMittens <matthewjg@mac.com>
2025-12-18 15:11:44 -0800

    Put SampleBankView in main view

--- SampleLauncher/ViewController.h
+++ SampleLauncher/ViewController.h
@@ -7,8 +7,12 @@
 
 #import <Cocoa/Cocoa.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface ViewController : NSViewController
 
 
 @end
+
+NS_ASSUME_NONNULL_END
 
--- SampleLauncher/ViewController.m
+++ SampleLauncher/ViewController.m
@@ -6,21 +6,39 @@
 //
 
 #import "ViewController.h"
+#import "SampleBankView.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) SampleBankView *bankView;
+
+@end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // Do any additional setup after loading the view.
-}
+    // Create and add sample bank view
+    self.bankView = [[SampleBankView alloc] initWithFrame:NSZeroRect capacity:16];
+    self.bankView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.bankView];
 
+    // Layout constraints - center in view with padding
+    [NSLayoutConstraint activateConstraints:@[
+        [self.bankView.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [self.bankView.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor],
+        [self.bankView.leadingAnchor constraintGreaterThanOrEqualToAnchor:self.view.leadingAnchor constant:20],
+        [self.bankView.trailingAnchor constraintLessThanOrEqualToAnchor:self.view.trailingAnchor constant:-20],
+        [self.bankView.topAnchor constraintGreaterThanOrEqualToAnchor:self.view.topAnchor constant:20],
+        [self.bankView.bottomAnchor constraintLessThanOrEqualToAnchor:self.view.bottomAnchor constant:-20],
+    ]];
+}
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
 }
-
 
 @end
